e1ee4e043414609e4095514a2976125735693c06
Shaun McDonald <shaun.mcdonald@xisystems.co.za>
2022-05-13 06:48:05 +0200

    chore(app): AppGlideModule default class
    Took 2 minutes

---
+++ app/src/main/java/tech/borgranch/pokedex/graphics/PokedexGlideModule.kt
@@ -0,0 +1,7 @@
+package tech.borgranch.pokedex.graphics
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class PokedexGlideModule : AppGlideModule()
